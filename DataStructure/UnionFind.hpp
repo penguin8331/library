dad1a336a9907ce31e621e6a6e58ca567cbf14ce
penguin8331 <shota50395123@outlook.jp>
2022-12-19 21:17:10 +0900

    Initial commit

--- DataStructure/UnionFind.hpp
+++ DataStructure/UnionFind.hpp
@@ -1,20 +1,3 @@
-//
-// Union-Find tree
-//
-
-/*
-    ・併合時の工夫: union by size
-    par[x]: x が根のときは x を含むグループのサイズ (の -1 倍)、そうでないときは親ノード
-
-    issame(x, y): x と y が同じグループにいるか, 計算量はならし O(α(n))
-    unite(x, y): x と y を同じグループにする, 計算量はならし O(α(n))
-    size(x): x を含むグループの所属メンバー数
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
-#pragma region UnionFind
 struct UnionFind {
     vector<int> par;
 
@@ -33,11 +16,11 @@ struct UnionFind {
         return root(x) == root(y);
     }
 
-    bool merge(int x, int y) {
+    bool unite(int x, int y) {
         x = root(x);
         y = root(y);
         if (x == y) return false;
-        if (par[x] > par[y]) swap(x, y);  // merge technique
+        if (par[x] > par[y]) swap(x, y);
         par[x] += par[y];
         par[y] = x;
         return true;
@@ -49,7 +32,7 @@ struct UnionFind {
 
     vector<vector<int>> groups() {
         map<int, vector<int>> root_buf;
-        for (int i = 0; i < par.size(); ++i) {
+        for (int i = 0; i < (int)par.size(); ++i) {
             int r = root(i);
             root_buf[r].push_back(i);
         }
@@ -60,4 +43,3 @@ struct UnionFind {
         return res;
     }
 };
-#pragma endregion UnionFind
---
+++ test/AOJ/UnionFind_Checker.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    UnionFind uf(N);
+    for (int i = 0; i < Q; i++) {
+        int com, x, y;
+        cin >> com >> x >> y;
+        if (com == 0) {
+            uf.unite(x, y);
+        } else {
+            cout << uf.issame(x, y) << endl;
+        }
+    }
+}
