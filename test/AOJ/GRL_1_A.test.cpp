e226f1254f0e15c21b83b90d08aed871349387a4
penguin8331 <111721385+penguin8331@users.noreply.github.com>
2022-12-20 23:12:01 +0900

    Update GRL_1_A.test.cpp

--- test/AOJ/GRL_1_A.test.cpp
+++ test/AOJ/GRL_1_A.test.cpp
@@ -13,6 +13,10 @@ int main() {
     }
     auto res = dijkstra(G, r);
     for (int i = 0; i < N; i++) {
-        cout << (res[i] == INF ? "INF" : res[i]) << endl;
+        if (res[i] == INF) {
+            cout << "INF" << endl;
+        } else {
+            cout << res[i] << endl;
+        }
     }
 }
