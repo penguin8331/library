12b0d80e7ff30a0f6a8a18068faa11a040440ae7
penguin8331 <111721385+penguin8331@users.noreply.github.com>
2022-12-20 22:50:08 +0900

    Update 0009.test.cpp

--- test/AOJ/0009.test.cpp
+++ test/AOJ/0009.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0009"
 #include "template/template.hpp"
-#include "data-structure/eratostenes"
+#include "data-structure/eratostenes.hpp"
 
 int main() {
     Eratos era(1e6);
