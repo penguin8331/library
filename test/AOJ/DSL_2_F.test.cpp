f6d7a6e7d647acaea2d370286f897458f25979f5
penguin8331 <shota50395123@outlook.jp>
2022-12-24 11:55:54 +0900

    fix

--- test/AOJ/DSL_2_F.test.cpp
+++ test/AOJ/DSL_2_F.test.cpp
@@ -8,7 +8,7 @@ int main() {
     auto fm = [](ll a, ll b) { return min(a, b); };
     auto fa = [](ll& a, ll d) { a = d; };
     auto fc = [](ll& d, ll e) { d = e; };
-    Lazy_SegTree<long long, long long> seg(N, fm, fa, fc, (1LL << 31) - 1, 0);
+    Lazy_SegTree<long long, long long> seg(N, fm, fa, fc, (1LL << 31) - 1, (1LL << 31) - 1);
     for (int i = 0; i < Q; i++) {
         int t;
         cin >> t;
---
+++ test/AOJ/DSL_2_G.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_G"
+#include "template/template.hpp"
+#include "data-structure/binary-indexed-tree-raq.hpp"
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    BIT<ll> bit(N);
+    for (int i = 0; i < Q; i++) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int l, r, v;
+            cin >> l >> r >> v;
+            bit.add(l, r + 1, v);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            cout << bit.sum(l, r + 1) << endl;
+        }
+    }
+}
