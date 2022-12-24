3f22ba3b455e25fe327c4169f6abd440d50f985b
penguin8331 <shota50395123@outlook.jp>
2022-12-24 11:15:17 +0900

    fix

---
+++ data-structure/binary-indexed-tree-raq.hpp
@@ -0,0 +1,44 @@
+template <class Abel>
+struct BIT {
+    Abel UNITY_SUM = 0;
+    vector<Abel> dat[2];
+
+    // [0, n)
+    BIT(int n, Abel unity = 0) : UNITY_SUM(unity) {
+        init(n);
+    }
+    void init(int n) {
+        for (int iter = 0; iter < 2; ++iter)
+            dat[iter].assign(n + 1, UNITY_SUM);
+    }
+
+    // [a, b), a and b are 0-indexed
+    inline void sub_add(int p, int a, Abel x) {
+        for (int i = a; i < (int)dat[p].size(); i |= i + 1)
+            dat[p][i] = dat[p][i] + x;
+    }
+    inline void add(int a, int b, Abel x) {
+        sub_add(0, a, x * (-a));
+        sub_add(1, a, x);
+        sub_add(0, b, x * b);
+        sub_add(1, b, x * (-1));
+    }
+
+    // [a, b), a and b are 0-indexed
+    inline Abel sub_sum(int p, int a) {
+        Abel res = UNITY_SUM;
+        for (int i = a - 1; i >= 0; i = (i & (i + 1)) - 1)
+            res = res + dat[p][i];
+        return res;
+    }
+    inline Abel sum(int a, int b) {
+        return sub_sum(0, b) + sub_sum(1, b) * b - sub_sum(0, a) - sub_sum(1, a) * a;
+    }
+
+    // debug
+    void print() {
+        for (int i = 0; i < (int)dat[0].size(); ++i)
+            cout << sum(i, i + 1) << ",";
+        cout << endl;
+    }
+};
---
+++ docs/binary-indexed-tree-raq.md
@@ -0,0 +1,4 @@
+---
+title: Binary Indexed Tree (区間加算 区間和取得に対応)
+documentation_of: //data-structure/binary-indexed-tree-raq.hpp
+---
