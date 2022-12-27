18484f2b12e133cafb1d194456c65c62f69c84c1
penguin8331 <shota50395123@outlook.jp>
2022-12-27 21:57:33 +0900

    geomeny

---
+++ test/AOJ/CGL_1_A.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_1_A"
+#define ERROR 0.00000001
+#include "template/template.hpp"
+#include "geomeny/projection.hpp"
+
+int main() {
+    Line a(2);
+    cin >> a[0].x >> a[0].y >> a[1].x >> a[1].y;
+    int Q;
+    cin >> Q;
+    for (int i = 0; i < Q; i++) {
+        Point b;
+        cin >> b.x >> b.y;
+        auto res = proj(b, a);
+        cout << res.x << " " << res.y << endl;
+    }
+}
---
+++ test/AOJ/CGL_7_E.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_7_E"
+#define ERROR 0.000001
+#include "template/template.hpp"
+#include "geomeny/crosspoint.hpp"
+
+int main() {
+    Circle a, b;
+    cin >> a.x >> a.y >> a.r >> a.x >> a.y >> a.r;
+    auto res = crosspoint(a, b);
+    sort(all(res), [](Point a, Point b) {
+        if (a.x != b.x) {
+            return a.x < b.x;
+        }
+        return a.y < b.y;
+    });
+    if (res.size() == 1) {
+        cout << res[0].x << " " << res[0].y << " " << res[0].x << " " << res[0].y << endl;
+    } else {
+        cout << res[0].x << " " << res[0].y << " " << res[1].x << " " << res[1].y << endl;
+    }
+}
