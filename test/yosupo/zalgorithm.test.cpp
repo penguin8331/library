c68a1410933b33e0830ec371be53494763c54369
penguin8331 <shota50395123@outlook.jp>
2023-01-08 18:12:02 +0900

    rolling hash

--- test/yosupo/zalgorithm.test.cpp
+++ test/yosupo/zalgorithm.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
 #include "template/template.hpp"
-#include "math/string/rolling-hash.hpp"
+#include "string/rolling-hash.hpp"
 
 int main() {
     string S;
