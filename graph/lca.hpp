32f5a07ccf029603a8ee58e7911b15b92245d3a2
penguin8331 <shota50395123@outlook.jp>
2023-01-30 18:54:49 +0900

    lca

--- graph/lca.hpp
+++ graph/lca.hpp
@@ -2,8 +2,8 @@ struct LCA {
     vector<vector<int>> parent;  // parent[d][v] := 2^d-th parent of v
     vector<int> depth;
     LCA() {}
-    LCA(const vector<vector<int>> &G, int r = 0) { init(G, r); }
-    void init(const vector<vector<int>> &G, int r = 0) {
+    LCA(const vector<vector<int>>& G, int r = 0) { init(G, r); }
+    void init(const vector<vector<int>>& G, int r = 0) {
         int V = (int)G.size();
         int h = 1;
         while ((1 << h) < V) ++h;
@@ -15,17 +15,23 @@ struct LCA {
                 if (parent[i][v] != -1)
                     parent[i + 1][v] = parent[i][parent[i][v]];
     }
-    void dfs(const vector<vector<int>> &G, int v, int p, int d) {
+    void dfs(const vector<vector<int>>& G, int v, int p, int d) {
         parent[0][v] = p;
         depth[v] = d;
         for (auto e : G[v])
             if (e != p) dfs(G, e, v, d + 1);
     }
+    int after(int u, int k) {
+        for (int i = 0; i < (int)parent.size(); i++) {
+            if (k & (1 << i)) {
+                u = parent[i][u];
+            }
+        }
+        return u;
+    }
     int get(int u, int v) {
         if (depth[u] > depth[v]) swap(u, v);
-        for (int i = 0; i < (int)parent.size(); ++i)
-            if ((depth[v] - depth[u]) & (1 << i))
-                v = parent[i][v];
+        v = after(v, depth[v] - depth[u]);
         if (u == v) return u;
         for (int i = (int)parent.size() - 1; i >= 0; --i) {
             if (parent[i][u] != parent[i][v]) {
@@ -34,5 +40,11 @@ struct LCA {
             }
         }
         return parent[0][u];
+    }
+    int dist(int u, int v) {
+        return depth[u] + depth[v] - 2 * depth[get(u, v)];
+    }
+    bool is_on_path(int u, int v, int a) {
+        return dist(u, a) + dist(a, v) == dist(u, v);
     }
 };
