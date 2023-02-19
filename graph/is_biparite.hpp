002c1b23d1ca6794dcbd533f33699e22795d1bc3
penguin8331 <shota50395123@outlook.jp>
2023-02-19 15:34:57 +0900

    Github Action Demo

---
+++ graph/is_biparite.hpp
@@ -0,0 +1,28 @@
+bool isbipartite(const vector<vector<int>>& G) {
+    int N = (int)G.size();
+    vector<int> color(N, -1);
+
+    for (int v = 0; v < N; ++v) {
+        if (color[v] != -1) {
+            continue;
+        }
+        queue<int> que;
+        color[v] = 0;
+        que.push(v);
+        while (!que.empty()) {
+            int qv = que.front();
+            que.pop();
+            for (auto nv : G[qv]) {
+                if (color[nv] != -1) {
+                    if (color[nv] == color[qv]) {
+                        return false;
+                    }
+                    continue;
+                }
+                color[nv] = 1 - color[qv];
+                que.push(nv);
+            }
+        }
+    }
+    return true;
+}
