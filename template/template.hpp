fc7d1cdb481b8916493447afcadee9880aad6c1a
penguin8331 <shota50395123@outlook.jp>
2023-02-24 16:02:12 +0900

    change template.hpp

--- template/template.hpp
+++ template/template.hpp
@@ -1,3 +1,4 @@
+#pragma region KCLC
 // #pragma GCC target("avx2")
 // #pragma GCC optimize("O3")
 // #pragma GCC optimize("unroll-loops")
@@ -9,16 +10,21 @@ using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 #define pb push_back
 #define mp make_pair
-#define mt make_tuple
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define elif else if
-#define updiv(N, X) ((N + X - 1) / X)
+#define updiv(N, X) (((N) + (X)-1) / (X))
 #define sigma(a, b) ((a + b) * (b - a + 1) / 2)
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define debug(...)
+#endif
 struct fast_ios {
     fast_ios() {
-        cin.tie(nullptr);
         ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         cout << fixed << setprecision(15);
     };
 } fast_ios_;
@@ -26,6 +32,8 @@ template <typename T>
 inline bool chmax(T& a, T b) { return ((a < b) ? (a = b, true) : (false)); }
 template <typename T>
 inline bool chmin(T& a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+#pragma endregion KCLC
+//----------------------------------------------------------------------------
 constexpr int inf = 1 << 30;
 constexpr ll INF = 1LL << 60;
 constexpr int dx[] = {1, 0, -1, 0, 1, -1, 1, -1};
