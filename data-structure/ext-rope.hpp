563de808cd9da834f9c437aaebd6e13a6e04403d
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2023-03-14 23:04:14 +0900

    commit

--- data-structure/ext-rope.hpp
+++ data-structure/ext-rope.hpp
@@ -3,13 +3,3 @@
 
 #include <ext/rope>
 using namespace __gnu_cxx;
-
-template<class T>
-ostream& operator<<(ostream& os, rope<T> A) {
-    os << "[ ";
-    for (const auto& i : A) {
-        os << i << " ";
-    }
-    os << "]";
-    return os;
-}
