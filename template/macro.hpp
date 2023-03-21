27d62a53082063ffaa66414a4dfdc154b24ab8b7
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2023-03-21 11:42:14 +0900

    change template

--- template/macro.hpp
+++ template/macro.hpp
@@ -2,8 +2,27 @@
 #include "../template/template.hpp"
 
 #define pb push_back
+#define eb emplace_back
 #define all(x) std::begin(x), std::end(x)
 #define rall(x) std::rbegin(x), std::rend(x)
 #define elif else if
 #define updiv(N, X) (((N) + (X) - (1)) / (X))
 #define sigma(a, b) ((a + b) * (b - a + 1) / 2)
+#define INT(...)     \
+    int __VA_ARGS__; \
+    scan(__VA_ARGS__)
+#define LL(...)     \
+    ll __VA_ARGS__; \
+    scan(__VA_ARGS__)
+#define STR(...)        \
+    string __VA_ARGS__; \
+    scan(__VA_ARGS__)
+#define CHR(...)      \
+    char __VA_ARGS__; \
+    scan(__VA_ARGS__)
+#define DOU(...)        \
+    double __VA_ARGS__; \
+    scan(__VA_ARGS__)
+#define LD(...)     \
+    ld __VA_ARGS__; \
+    scan(__VA_ARGS__)
