3493802631ac5619d5b5ba0253ae4877a0acdf53
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2023-05-20 19:54:32 +0900

    add rand-int

--- others/rand-int.hpp
+++ others/rand-int.hpp
@@ -1,13 +1,24 @@
 #pragma once
 #include "../template/template.hpp"
 
-struct ran {
+struct rand_int {
     mt19937 mt;
-    ran() {
+    rand_int() {
         mt.seed(std::chrono::system_clock::now().time_since_epoch().count());
     }
     ll get(ll A, ll B) {
         uniform_int_distribution<ll> dist(A, B);
         return dist(mt);
+    }
+    vector<int> get_permutation(int N) {
+        vector<int> ret(N);
+        for (int i = 0; i < N; i++) {
+            ret[i] = i;
+        }
+        for (int i = N - 1; i >= 0; i--) {
+            int j = get(i, N - 1);
+            swap(ret[i], ret[j]);
+        }
+        return ret;
     }
 };
