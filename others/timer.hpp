ee411be60548fab483fe7eb4dd234e818192b6ef
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2023-06-11 20:11:36 +0900

    add timer.hpp

---
+++ others/timer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "../template/template.hpp"
+
+struct Timer {
+    chrono::high_resolution_clock::time_point start, end;
+    double limit;
+
+    Timer() { start = chrono::high_resolution_clock::now(); }
+    Timer(double l) {
+        start = chrono::high_resolution_clock::now();
+        limit = l;
+    }
+    double get_time() {
+        end = chrono::high_resolution_clock::now();
+        return chrono::duration<double>(end - start).count();
+    }
+    bool is_over() { return get_time() > limit; }
+    bool is_under() { return get_time() <= limit; }
+    void set_limit(double l) { limit = l; }
+    void set_start() { start = chrono::high_resolution_clock::now(); }
+};
