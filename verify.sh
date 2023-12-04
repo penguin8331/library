7718d0ae1b5a0c32c40dbfd6b905f305ed9fe981
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2023-12-05 00:03:33 +0900

    commit

--- verify.sh
+++ verify.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 export $(cat .env| grep -v "#" | xargs)
+export DROPBOX_TOKEN=$(
+curl -s https://api.dropbox.com/oauth2/token \
+-u ${DROPBOX_APP_KEY}:${DROPBOX_APP_SECRET} \
+-d grant_type=refresh_token \
+-d refresh_token=${DROPBOX_REFRESH_TOKEN} \
+| jq -r '.access_token'
+)
 oj-verify all
