75f93e0020c20bea4fc373b98eaa8328218b73a1
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2024-03-30 19:39:33 +0900

    commit

--- test/AOJ/DSL_2_H.test.cpp
+++ test/AOJ/DSL_2_H.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_I"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H"
 #include "../../data-structure/lazy-segment-tree.hpp"
 #include "../../template/template.hpp"
 
