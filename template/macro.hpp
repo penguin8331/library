00192c684ba239e6179ad886a53922004d29ea66
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2024-07-12 09:17:29 +0900

    change all, rall

--- template/macro.hpp
+++ template/macro.hpp
@@ -1,11 +1,18 @@
 #pragma once
 #include "../template/template.hpp"
 
-#define all(x) std::begin(x), std::end(x)
-#define rall(x) std::rbegin(x), std::rend(x)
+#define overload3(_1, _2, _3, name, ...) name
+#define all1(v) std::begin(v), std::end(v)
+#define all2(v, a) std::begin(v), std::begin(v) + a
+#define all3(v, a, b) std::begin(v) + a, std::begin(v) + b
+#define all(...) overload3(__VA_ARGS__, all3, all2, all1)(__VA_ARGS__)
+#define rall1(v) std::rbegin(v), std::rend(v)
+#define rall2(v, a) std::rbegin(v), std::rbegin(v) + a
+#define rall3(v, a, b) std::rbegin(v) + a, std::rbegin(v) + b
+#define rall(...) overload3(__VA_ARGS__, rall3, rall2, rall1)(__VA_ARGS__)
 #define elif else if
-#define updiv(N, X) (((N) + (X) - (1)) / (X))
-#define sigma(a, b) ((a + b) * (b - a + 1) / 2)
+#define updiv(N, X) (((N) + (X) - 1) / (X))
+#define sigma(a, b) (((a) + (b)) * ((b) - (a) + 1) / 2)
 #define INT(...)     \
     int __VA_ARGS__; \
     scan(__VA_ARGS__)
